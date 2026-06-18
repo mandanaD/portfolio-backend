91115835b47f2ec1be6df7a6eb2f079b9a1dd3e7
Mandana Dashti <dmandana59@gmail.com>
2026-06-18 19:04:20 +0330

    feat: create UpdateUserDto

---
+++ src/module/user/dtos/update-user.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsEnum(['VISITOR', 'OWNER'])
+  role?: string;
+}
