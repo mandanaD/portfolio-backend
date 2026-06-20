44935c35895108f04d49a9b3063292dd449003b6
Mandana Dashti <dmandana59@gmail.com>
2026-06-20 18:12:14 +0330

    feat: add patch, delete and get

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -1,13 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // get all and profile edit and get
+
+  @Get(':id')
+  getUser(@Param('id') id: string) {
+    return this.userService.getUser(id);
+  }
+
   @Post()
   createUser(@Body() body: CreateUserDto) {
     return this.userService.createUser(body);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.userService.updateUser(id, body);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
   }
 }
