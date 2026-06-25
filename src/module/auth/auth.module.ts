eb4ecb0281ba6494e520028ace4374d763aff87c
Mandana Dashti <dmandana59@gmail.com>
2026-06-25 20:54:49 +0330

    feat: add import

--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt/jwt.strategy';
+import { UserModule } from '../user/user.module';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('SECRET_AUTH_KEY'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
