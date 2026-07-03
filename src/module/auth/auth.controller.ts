d6c7140816cecaa0e447f3dc8a95a9951606f0ca
Mandana Dashti <dmandana59@gmail.com>
2026-07-03 18:23:03 +0330

    feat: create logout & refresh

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -2,18 +2,34 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dtos/signup.dto';
 import { LoginDto } from './dtos/login.dto';
+import { Public } from '../../decotaror/public.decorator';
+import { RefreshDto } from './dtos/refresh.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('/signup')
   signup(@Body() body: SignupDto) {
     return this.authService.signup(body);
   }
 
+  @Public()
   @Post('/login')
   login(@Body() body: LoginDto) {
     return this.authService.login(body);
+  }
+
+  @Public()
+  @Post('/refresh')
+  refresh(@Body() body: RefreshDto) {
+    return this.authService.refresh(body.refresh_token);
+  }
+
+  @Public()
+  @Post('/logout')
+  logout(@Body() body: RefreshDto) {
+    return this.authService.logout(body.refresh_token);
   }
 }
