9397b1ae7eea24c5daa114fb27381415cc860746
Mandana Dashti <dmandana59@gmail.com>
2026-07-03 18:31:36 +0330

    feat: add ApiProperty

--- src/module/user/dtos/update-user.dto.ts
+++ src/module/user/dtos/update-user.dto.ts
@@ -5,26 +5,34 @@ import {
   IsString,
   MinLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
+  @ApiProperty()
   firstName?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty()
   lastName?: string;
 
   @IsOptional()
   @IsEmail()
+  @ApiProperty()
   email?: string;
 
   @IsOptional()
   @IsString()
   @MinLength(6)
+  @ApiProperty()
   password?: string;
 
   @IsOptional()
   @IsEnum(['VISITOR', 'OWNER'])
+  @ApiProperty({
+    enum: ['VISITOR', 'OWNER'],
+  })
   role?: string;
 }
