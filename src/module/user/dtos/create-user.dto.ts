f67f9fd301184b2539f0de41271b5d17ab357b4b
Mandana Dashti <dmandana59@gmail.com>
2026-07-03 18:24:15 +0330

    feat: add enum add api property

--- src/module/user/dtos/create-user.dto.ts
+++ src/module/user/dtos/create-user.dto.ts
@@ -1,8 +1,12 @@
 import { IsEnum, IsOptional } from 'class-validator';
 import { SignupDto } from '../../auth/dtos/signup.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto extends SignupDto {
   @IsOptional()
   @IsEnum(['VISITOR', 'OWNER'])
+  @ApiProperty({
+    enum: ['VISITOR', 'OWNER'],
+  })
   role: string;
 }
