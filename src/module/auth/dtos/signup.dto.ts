6518dbe1e4adbc0cf91b9ea19600252054fbef4a
Mandana Dashti <dmandana59@gmail.com>
2026-07-03 18:31:30 +0330

    feat: add ApiProperty

--- src/module/auth/dtos/signup.dto.ts
+++ src/module/auth/dtos/signup.dto.ts
@@ -1,16 +1,21 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignupDto {
   @IsString()
+  @ApiProperty()
   firstName: string;
 
   @IsString()
+  @ApiProperty()
   lastName: string;
 
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @MinLength(6)
+  @ApiProperty()
   password: string;
 }
