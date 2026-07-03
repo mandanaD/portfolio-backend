0571f9c672e901d5df7e35bd3c0b9e4d945a515d
Mandana Dashti <dmandana59@gmail.com>
2026-07-03 18:24:25 +0330

    feat: add api property

--- src/module/auth/dtos/login.dto.ts
+++ src/module/auth/dtos/login.dto.ts
@@ -1,12 +1,15 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsNotEmpty()
   @IsString()
   @MinLength(6)
+  @ApiProperty()
   password: string;
 }
